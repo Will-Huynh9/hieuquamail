d48deccd42572b1a0c0ed574a087459a457ffd35
Will-Huynh9 <huynhphutuan99@gmail.com>
2024-11-15 16:56:59 +0700

    Update pixel.js

--- api/pixel.js
+++ api/pixel.js
@@ -3,26 +3,27 @@ import { google } from 'googleapis';
 const auth = new google.auth.GoogleAuth({
   credentials: {
     client_email: process.env.CLIENT_EMAIL,
-    private_key: (process.env.PRIVATE_KEY_P1 + process.env.PRIVATE_KEY_P2).replace(/\\\\n/g, '\n'),  },
+    private_key: (process.env.PRIVATE_KEY_P1 + process.env.PRIVATE_KEY_P2).replace(/\\\\n/g, '\n'),
+  },
   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
 
 const sheets = google.sheets({ version: 'v4', auth });
 
 export default async function handler(req, res) {
-  const { email } = req.query; // Lấy email từ query parameter
+  const { email, type } = req.query; // Lấy email và type từ query parameter
   const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
   const userAgent = req.headers['user-agent'];
   const timestamp = new Date().toISOString();
 
-  console.log(`Tracking pixel loaded - IP: ${ip}, User-Agent: ${userAgent}, Time: ${timestamp}`);
+  console.log(`Tracking pixel loaded - Email: ${email}, Type: ${type}, IP: ${ip}, User-Agent: ${userAgent}, Time: ${timestamp}`);
 
   try {
     const spreadsheetId = process.env.GOOGLE_SHEET_ID; // ID của Google Sheet từ biến môi trường
-    const range = 'Email03!A:D'; // Vùng mà bạn muốn ghi dữ liệu vào
+    const range = 'Email03!A:E'; // Thêm một cột mới cho type
 
     const values = [
-      [email, timestamp, ip, userAgent], // Dữ liệu cần ghi
+      [email, timestamp, ip, userAgent, type], // Ghi dữ liệu với type
     ];
 
     const resource = {
